35d758abcfedbb1af1ec7baa4ebad1a8baa5a82c
Pushpesh Kumar <krpushpesh108@gmail.com>
2023-03-11 21:56:39 +0530

    Step 8: Filter and Results page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import './style.css';
+import HomePage from './pages/HomePage';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Layout from './pages/Layout';
-import HomePage from './pages/HomePage';
 import Login from './pages/Login';
+import AirportAvailability from './pages/AirportAvailability'
+
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-   <Route path="/" element={<Layout/>}>
-   <Route path="/" index element={<HomePage/>}/>
-   <Route path="/login" element={<Login/>}/>
-   <Route path="*" element={<HomePage/>}/>
-   
-   </Route>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter >
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="results" element={<AirportAvailability />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<HomePage />} />
+    </Route>
+  </Routes>
+  </BrowserRouter >
   );
 }
 
--- frontend/src/pages/SearchForm.js
+++ frontend/src/pages/SearchForm.js
@@ -1,166 +1,188 @@
-import { useEffect } from "react";
-import React,{ useState } from "react";
+import React,{useState,useEffect} from "react";
 import moment from "moment";
 import axios from "axios";
-
+import AirportSuggetions from "./AirportSuggestions";
 const SearchForm = () => {
-    const today = moment().format('YYYY-MM-DD').toString();
-    const tomrrow = moment().add(1, 'days').format('YYYY-MM-DD').toString();
-    const [departureAirport, setDepartureAirport] = useState("");
-    const [checkin, setCheckin] = useState(today);
-    const [checkout, setCheckout] = useState(tomrrow);
-    const [errors, seterrors] = useState({
-        departureAirport: false,
-        checkin: false,
-        checkout: false
-    })
 
-
-    const[records,setRecords]=useState([]);
-    const[loading,setLoading]=useState(false);
-    const fetchData=async()=>{
-     setLoading(true)
-     const {data}=await axios.get('http://43.205.1.85:9009/v1/airports')
-     setLoading(false)
-     setRecords(data.results)
-    }
-    useEffect(()=>{
-     fetchData()
-    },[])
-
-
-
-
-
-    const DepartureAirportHandler = (e) => {
-        //event.target.value    
-
-        const { value } = e.target;
-
-        setDepartureAirport(value);
-        if (e.target) {         
-
-            seterrors({ ...errors, departureAirport: false })
-
-        }
-
-    }
-
+    const [airports, setAirports] = useState([]);
+    const [filteredAirports, setFilteredAirports] = useState('');
     
-    const checkinHandler = (e) => {
-        //event.target.value
-        const { value } = e.target;
-        setCheckin(value);
-        if (e.target) {
-            seterrors({ ...errors, checkin: false })
+    const getAirport = async () => {
+        
+        try {
+            const { data, status } = await axios.get('http://43.205.1.85:9009/v1/airports');
+            if (status === 200 && data) {
+                setAirports(data?.results ?? [])
+            } else {
+                setAirports([])
+            }
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+            console.log(error.message)
         }
-
-    }
-    const checkoutHandler = (e) => {
-        //event.target.value
-        const { value } = e.target;
-        setCheckout(value);
-        if (moment(checkin) > moment(checkout)) {
-            seterrors((err) => ({ ...err, checkout: true }))
-
-        }
-        if (e.target.value) {
-            seterrors((err) => ({ ...err, checkout: false }))
-        }
-        else {
-            seterrors((err) => ({ ...err, checkout: true }))
-        }
-
     }
 
-    const submitHandler = (e) => {
-        console.log(departureAirport)
-        console.log(checkin)
-        console.log(checkout)
-        //event.target.value
+    useEffect(() => {
+        
+        getAirport()
+    }, [])
+
+    const [loading, setLoading] = useState(true);
+    
+    const selectAirport =(value)=>{
+        setDepartureAirport(value)
+        setFilteredAirports([])
+    }
+
+    const today = moment().format('YYYY-MM-DD').toString()
+    const tomorrow = moment().add(1,'days').format('YYYY-MM-DD').toString()
+    const[departureAirport,setDepartureAirport ]= useState('');
+    const[parkingCheckIn,setParkingCheckIn ]= useState(today);
+    const[parkingCheckOut,setParkingCheckOut ]= useState(tomorrow);
+    const[errors,SetErrors]=useState({
+        departureAirport:false,
+        parkingCheckIn:false,
+        parkingCheckOut:false
+    })
+    
+    const onSubmitHandler = (e) => {
         e.preventDefault();
-        if (moment(checkin) > moment(checkout)) {
-            alert("Please check the date once again")
-            seterrors((err) => ({ ...err, checkout: true }))
+        //i'm printing all the data fetched in console just to verify everything works fine
+        console.log(departureAirport);
+        console.log(parkingCheckIn);
+        console.log(parkingCheckOut);
+
+        if(moment(parkingCheckIn) > moment(parkingCheckOut))
+        {
+            alert("Check In Date can't be greater than Check Out Date")
+            SetErrors((err) => ({ ...err, parkingCheckOut: true }))
 
         }
 
-        else if (departureAirport && checkin && checkout) {
-            alert("Submitted")
+       else if(departureAirport && parkingCheckIn && parkingCheckOut)
+        {
+            alert("Form Submitted")
+            window.location.href = `/results?departureAirport=${departureAirport}&checkin=${parkingCheckIn}&checkout=${parkingCheckOut}`
+
         }
-        else {
-            seterrors({
-                departureAirport: !departureAirport,
-                checkin: !checkin,
-                checkout: !checkout
+        else{
+            SetErrors({
+                departureAirport:!departureAirport,
+                parkingCheckIn:!parkingCheckIn,
+                parkingCheckOut:!parkingCheckOut
             })
         }
-
     }
-    return (
-        <div>
-            <form action="/results.html" method="post" autoComplete="off" noValidate="">
-                <div className="options row m-0"><label className="col-12 col-xl-3 p-0 mr-xl-3 mb-2">
-                    <div className="heading mb-1">Departure Airport</div>
-                    <div className="placeholder placeholder-airport">
-                        <input type="text" onChange={DepartureAirportHandler}
-                            value={departureAirport}
-                            placeholder="Departure Airport"
-                            className="placeholder placeholder-airport" />
-                        {(errors.departureAirport ? <div><br /><div style={{ border: 1, backgroundColor: "#00FFFF" }}><h6><em>Invalid Departure Airport</em></h6></div></div> : null)}
-                    </div> 
-                    
-                    
-                    <ul>
-                                   {records.map((record,index)=>{
-                                     const isEven = index%2;
-                                       return (
-                                         <li key={index}style={{backgroundColor:isEven?'black':'grey'}}>
-                                             {record.name}
-                                         </li>
-                                              )
-                                             }
-                                         )} 
-                                     </ul>
 
-                    
-                    
-                    
-                    
-                    <i
-                        className="fas fa-map-marker-alt input-icon"></i>
-                </label>
-                    <div className="col p-0 row m-0 mb-2 dates"><label
-                        className="col-sm-6 p-0 pr-sm-3 date_input">
-                        <div className="heading mb-1">Parking Check-In</div>
-                        <div className="placeholder">
-                            <input name="checkin"
-                                type="date"
-                                onChange={checkinHandler}
-                                placeholder="Parking Check-Out"
-                                value={checkin}
-                                className="placeholder placeholder-airport"
-                                style={{ width: '100%' }} />
-                            {(errors.checkin ? <div><br /><div style={{ border: 1, backgroundColor: "#00FFFF" }}><h6><em>Invalid check-in date</em></h6></div></div> : null)}
+    const parkingCheckOutHandler =(e) => {
+        const {value}=e.target;
+            setParkingCheckOut(value);
+            if(moment(parkingCheckIn) > moment(parkingCheckOut))
+            {
+                SetErrors((err) => ({ ...err, parkingCheckOut: true }))
+    
+            }
+            if (e.target.value) {
+                SetErrors((err) => ({ ...err, parkingCheckOut: false }))
+                } 
+            else {
+                SetErrors((err) => ({ ...err, parkingCheckOut: true }))
+                }
+                
+    }
+    const parkingCheckInHandler =(e) => {
+        const {value}=e.target;
+            setParkingCheckIn(value);
+           
+            if (e.target.value) {
+                SetErrors((err) => ({ ...err, parkingCheckIn: false }))
+                } else {
+                SetErrors((err) => ({ ...err, parkingCheckIn: true }))
+                }
+    }
+    const departureAirportHandler =(e) => {
+        const {value}=e.target;
+        if(value.length<15){
+            setDepartureAirport(value);
+        }
+        
+        if (e.target.value) {
+            SetErrors((err) => ({ ...err, departureAirport: false }))
+            } else {
+            SetErrors((err) => ({ ...err, departureAirport: true }))
+            }
+            const filteredAirportsData = airports.filter((airport) => 
+            airport.name.toLowerCase().includes(e.target.value.toLowerCase()));
+            setFilteredAirports(filteredAirportsData?? [])
+            console.log(filteredAirports)
+    }
+    
+    return (
+        <section id="hero"
+        style={{backgroundImage: 'url("assets/generic_landing.jpg")', minHeight: '500px'}}>
+        <div className="hero-backdrop"></div>
+        <div className="container position-relative">
+            <div className="hero-heading mb-4">
+                <h1>SAVE BIG ON AIRPORT PARKING</h1>
+                <h2>We have the best deals for airport parking lots!</h2>
+            </div>
+            <div className="searchbox landing">
+                <div className="row tabs">
+                    <div className="tab">
+                        <div className="heading">Most Convenient</div>
+                        <div className="button">
+                            <div className="icon"><i className="fas fa-car"></i></div>
+                            Airport Parking Only
                         </div>
-                    </label> <label className="col-sm-6 p-0 pl-sm-0 date_input">
-                            <div className="heading mb-1">Parking Check-Out</div>
-                            <input name="Check-Out"
-                                type="date"
-                                onChange={checkoutHandler}
-                                placeholder="Parking Check-Out"
-                                value={checkout}
-                                className="placeholder placeholder-airport"
-                                style={{ width: "100%" }} />
-                            {(errors.checkout ? <div><br /><div style={{ border: 1, backgroundColor: "#00FFFF" }}><h6><em>Invalid check-out date</em></h6></div></div> : null)}
-                        </label></div>
-                    <div className="col-12 col-xl-2 p-0 pl-xl-3 my-3 my-xl-0">
-                        <div className="d-none d-xl-block heading mb-1 invisible">Submit</div>
-                        <button type="submit" onClick={submitHandler} className="btn btn-secondary btn-big btn-block p-2"><span>SEARCH</span></button>
                     </div>
+                    <div className="tab">
+                        <div className="heading">Best Value</div>
+                        <div className="button">
+                            <div className="icon"><i className="fas fa-bed"></i> + <i
+                                    className="fas fa-car"></i></div>
+                            Hotel &amp; Parking Package
+                        </div>
+                    </div> 
                 </div>
-            </form>
+                {loading && <h3>loading..</h3>}
+                <form action="/results.html" method="post" >
+                    <div className="options row m-0"><label className="col-12 col-xl-3 p-0 mr-xl-3 mb-2">
+                            <div className="heading mb-1">Departure Airport</div>
+                            <div className="placeholder placeholder-airport">
+                                <input type="text" placeholder="Departure Airport" 
+                                onChange={departureAirportHandler} 
+                                value={departureAirport} className="placeholder placeholder-airport"/>
+                                
+                            </div> <i
+                                className="fas fa-map-marker-alt input-icon"></i>
+                            
+                            {loading ?<h1>Loading</h1>:null}
+                            {(errors && errors.departureAirport)? <h4 style={{color:"white",backgroundColor:"Highlight"}}>Invaild Departure Airport</h4>:null}
+                            <AirportSuggetions airports={filteredAirports} selectAirport={selectAirport} />
+
+                        </label>
+                        <div className="col p-0 row m-0 mb-2 dates"><label
+                                className="col-sm-6 p-0 pr-sm-3 date_input">
+                                <div className="heading mb-1">Parking Check-In</div>
+                                <div className="placeholder">
+                                    <input name="checkin" type="date" placeholder="Parking Check-Out" onChange={parkingCheckInHandler} value={parkingCheckIn} className="placeholder placeholder-airport" style={{width:"100%"}}/>
+                                {(errors && errors.parkingCheckIn)? <h2 style={{color: "white",backgroundColor:"Highlight"}}>Invaild Parking Check-In</h2>:null}
+                                </div> 
+                            </label> <label className="col-sm-6 p-0 pl-sm-0 date_input">
+                                <div className="heading mb-1">Parking Check-Out</div>
+                                    <input name="Check-Out" type="date" placeholder="Parking Check-Out" onChange={parkingCheckOutHandler} value={parkingCheckOut} className="placeholder placeholder-airport" style={{width:"100%"}}/>
+                                {(errors && errors.parkingCheckOut)? <h2 style={{color: "white",backgroundColor:"Highlight"}}>Invaild Parking Check-Out</h2>:null}
+                            </label></div>
+                        <div className="col-12 col-xl-2 p-0 pl-xl-3 my-3 my-xl-0">
+                            <div className="d-none d-xl-block heading mb-1 invisible">Submit</div>
+                            <button type="submit" onClick={onSubmitHandler} className="btn btn-secondary btn-big btn-block p-2"><span>SEARCH</span></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
+    </section>
     );
 }
 export default SearchForm;
